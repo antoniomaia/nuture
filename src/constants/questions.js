e579dc71b4f1d25f0cb7d3b1689efe8a09a8b1b4
antoniomaia <antoniobcmaia@gmail.com>
2020-04-10 12:45:49 +0100

    add more questions

--- src/constants/questions.js
+++ src/constants/questions.js
@@ -1,9 +1,25 @@
 export const questions = [
   {
+    title: "Country you live in?",
+    choices: ["Country"]
+  },
+  {
     title: "Select the transport mode you use most often",
     description: "Some stuff about diet affecting carbon emissions",
     choices: ["Car", "Bus", "Train", "Motorbike", "Bicycle", "Walk"],
     type: "single"
+  },
+  {
+    title: "On average how much time per day do you spend traveling?",
+    description: "Some stuff about diet affecting carbon emissions",
+    choices: [""],
+    type: "number"
+  },
+  {
+    title: "How many roundtrip flights do you take each year?",
+    description: "Some stuff about diet affecting carbon emissions",
+    choices: [""],
+    type: "number"
   },
   {
     title: "What is your diet like?",
@@ -16,5 +32,13 @@ export const questions = [
       "Meat in every meal"
     ],
     type: "single"
+  },
+  {
+    title: "How much do you use in your home?",
+    choices: ["Electricity", "Natural Gas"]
+  },
+  {
+    title: "How much do you spend on new stuff monthly?",
+    choices: ["Shopping", "New Stuff"]
   }
 ];
