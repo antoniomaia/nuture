6fbbf6ea2c4889a530e81078658abf5b1dc522bc
antoniomaia <antoniobcmaia@gmail.com>
2020-04-20 15:52:33 +0100

    implement scroll restoration and disabled pwa

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import ScrollRestoration from './components/global/scroll-restoration';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
+      <ScrollRestoration />
       <App />
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorker.unregister();
