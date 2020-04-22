fd2d114bf7c83ba506b147816a1231b127a484c4
antoniomaia <antoniobcmaia@gmail.com>
2020-04-22 01:09:23 +0100

    implement google analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import App from './App';
 import ScrollRestoration from './components/global/scroll-restoration';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('UA-164078398-1');
 
 ReactDOM.render(
   <BrowserRouter>
