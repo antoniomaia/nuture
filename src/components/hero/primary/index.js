491e3f27dc8384a7300e1109e633087630b35a27
antoniomaia <antoniobcmaia@gmail.com>
2020-10-24 18:09:25 +0100

    add header background

--- src/components/hero/primary/index.js
+++ src/components/hero/primary/index.js
@@ -6,22 +6,28 @@ import styles from './styles.module.scss';
 
 const Primary = () => {
   return (
-    <section className={cx(styles.root, styles.hero)}>
-      <ul>
-        <li>Measure</li>
-        <li>Reduce</li>
-        <li>Offset</li>
-        <li>Share</li>
-      </ul>
-      <h1 className={styles.headline}>your company <br/>carbon footprint</h1>
-      <p className={styles.sub_heading}>
-        All the tools you need to start your path to carbon neutral are here!
-      </p>
-      <div className={styles.cta_container}>
-        <Link className={styles.button} to={'/calculator'}>
-          Get a quote
-        </Link>
+    <section className={styles.hero_container}>
+      <div className={cx(styles.root, styles.hero)}>
+        <ul>
+          <li>Measure</li>
+          <li>Reduce</li>
+          <li>Offset</li>
+          <li>Share</li>
+        </ul>
+        <h1 className={styles.headline}>
+          your company <br />
+          carbon footprint
+        </h1>
+        <p className={styles.sub_heading}>
+          All the tools you need to start your path to carbon neutral are here!
+        </p>
+        <div className={styles.cta_container}>
+          <Link className={styles.button} to={'/calculator'}>
+            Get a quote
+          </Link>
+        </div>
       </div>
+      <div className={styles.waves}></div>
     </section>
   );
 };
