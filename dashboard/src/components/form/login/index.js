fd8d0a33cac9a22a7a9acb700860831ab2ff6d94
antoniomaia <antoniobcmaia@gmail.com>
2020-11-01 16:09:44 +0000

    implement form validations for login

--- dashboard/src/components/form/login/index.js
+++ dashboard/src/components/form/login/index.js
@@ -8,64 +8,98 @@ import { login } from 'slices/auth';
 import { useSelector } from 'react-redux';
 import { STATUS } from 'constants/status';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { useForm, Form } from 'components/form/useForm';
+import Controls from '../controls';
+
+const initialFormValues = {
+  email: '',
+  password: '',
+};
 
 const LoginForm = () => {
+  const validate = (fieldValues = values) => {
+    const temp = {};
+    temp.email = values.email ? '' : 'This field is required.';
+    temp.password = values.password ? '' : 'This field is required.';
+
+    setErrors({
+      ...temp,
+    });
+
+    if (fieldValues === values) return Object.values(temp).every(x => x === '');
+  };
+
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetForm,
+  } = useForm(initialFormValues, true, validate);
+
   const status = useSelector(state => state.auth.status);
   const isLogginIn = status === STATUS.LOADING;
   const dispatch = useAppDispatch();
 
   const onLogin = e => {
     e.preventDefault();
-    dispatch(
-      login({
-        email: 'eve.holt@reqres.in',
-        password: 'cityslicka',
-      })
-    );
+    if (validate()) {
+      debugger
+      dispatch(
+        login({
+          email: 'eve.holt@reqres.in',
+          password: 'cityslicka',
+        })
+      );
+      //resetForm();
+    }
   };
 
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minWidth: 200,
-          maxWidth: 300,
-          width: '100%',
-        }}
-      >
-        <Typography align="center" variant="h4" gutterBottom>
-          Login
-        </Typography>{' '}
-        <TextField
-          label="Email"
-          type="email"
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          label="Password"
-          type="password"
-          margin="normal"
-          variant="outlined"
-        />
-        <div style={{ height: '1rem' }} />
-        <Button
-          color="primary"
-          variant="contained"
-          size="large"
-          onClick={onLogin}
-          disabled={isLogginIn}
+    <Form onSubmit={onLogin}>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 200,
+            maxWidth: 300,
+            width: '100%',
+          }}
         >
-          {!isLogginIn ? 'Log in' : <CircularProgress size={30} />}
-        </Button>
-        <div style={{ height: '1rem' }} />
-        <Button href="/register" size="large">
-          No account? Create one
-        </Button>
-      </div>
-    </Grid>
+          <Typography align="center" variant="h4" gutterBottom>
+            Login
+          </Typography>
+          <Controls.Input
+            name="email"
+            label="Email"
+            value={values.email}
+            onChange={handleInputChange}
+            error={errors.email}
+          />
+          <Controls.Input
+            name="password"
+            label="Password"
+            type="password"
+            value={values.password}
+            onChange={handleInputChange}
+            error={errors.password}
+          />
+          <div style={{ height: '1rem' }} />
+          <Controls.Button
+            type="submit"
+            text={!isLogginIn ? 'Log in' : <CircularProgress size={30} />}
+          />
+          <Controls.Button
+            href="/register"
+            variant="default"
+            text="No account? Create one"
+            lowercase
+          />
+        </div>
+      </Grid>
+    </Form>
   );
 };
 
--- dashboard/src/components/form/register/index.js
+++ dashboard/src/components/form/register/index.js
@@ -2,45 +2,105 @@ import React from 'react';
 import Grid from '@material-ui/core/grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+
+import { useForm, Form } from 'components/form/useForm';
+import Controls from 'components/form/controls';
+
+const initialFormValues = {
+  companyName: '',
+};
 
 const LoginForm = () => {
+  const validate = (fieldValues = values) => {
+    const temp = {};
+    temp.companyName = values.companyName ? '' : 'This field is required.';
+    setErrors({
+      ...temp,
+    });
+
+    if (fieldValues === values) return Object.values(temp).every(x => x === '');
+  };
+
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetForm,
+  } = useForm(initialFormValues, true, validate);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (validate()) {
+      //employeeService.insertEmployee(values);
+      //resetForm();
+    }
+  };
+
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minWidth: 200,
-          maxWidth: 300,
-          width: '100%',
-        }}
-      >
-        <Typography align="center" variant="h4" gutterBottom>
-          Register
-        </Typography>
-        <TextField
-          label="Email"
-          type="email"
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          label="Password"
-          type="password"
-          margin="normal"
-          variant="outlined"
-        />
-        <div style={{ height: '1rem' }} />
-        <Button color="primary" variant="contained" size="large">
-          Create Account
-        </Button>
-        <div style={{ height: '1rem' }} />
-        <Button href="/login" size="large">
-          Already have an account?
-        </Button>
-      </div>
-    </Grid>
+    <Form onSubmit={handleSubmit}>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 200,
+            maxWidth: 300,
+            width: '100%',
+          }}
+        >
+          <Typography align="center" variant="h4" gutterBottom>
+            Register
+          </Typography>
+          <Controls.Input
+            name="companyName"
+            label="Company name"
+            value={values.companyName}
+            onChange={handleInputChange}
+            error={errors.companyName}
+          />
+          <TextField
+            label="Economic sector"
+            type="text"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            label="Work email"
+            type="text"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            label="Number of full-time employees"
+            type="text"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            label="Email"
+            type="email"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            margin="normal"
+            variant="outlined"
+          />
+          <div style={{ height: '1rem' }} />
+          <Controls.Button type="submit" text="Create Account" />
+          <Controls.Button
+            href="/login"
+            variant="default"
+            text="Already have an account?"
+            lowercase
+          />
+        </div>
+      </Grid>
+    </Form>
   );
 };
 
