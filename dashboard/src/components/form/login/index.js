cdf636c626453476f1d53b155897c7b433a4a832
antoniomaia <antoniobcmaia@gmail.com>
2020-11-01 22:06:09 +0000

    fix import

--- dashboard/src/components/form/login/index.js
+++ dashboard/src/components/form/login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Grid from '@material-ui/core/grid';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { useAppDispatch } from 'store';
 import { login } from 'slices/auth';
