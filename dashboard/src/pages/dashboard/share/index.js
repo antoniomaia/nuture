a0a58ebfd09455796eed7a92e86c8f755a5857e7
antoniomaia <antoniobcmaia@gmail.com>
2020-11-04 22:32:10 +0000

    proof of concept for badge

--- dashboard/src/pages/dashboard/share/index.js
+++ dashboard/src/pages/dashboard/share/index.js
@@ -1,7 +1,32 @@
-const Share = (props) => {
+import { useEffect, useState } from 'react';
+
+const loadEcologicalEarthBadge = () => {
+  const existingScript = document.getElementById('ecologicalEarth');
+  if (!existingScript) {
+    const script = document.createElement('script');
+    script.src = 'http://local.ecological.earth:8080/badge.js';
+    script.id = 'ecologicalEarth';
+    document.body.appendChild(script);
+  }
+};
+
+const Share = () => {
+  const [companyId, setCompanyId] = useState(1);
+
+  useEffect(() => {
+    loadEcologicalEarthBadge();
+  });
+
   return (
     <div>
       <h1>Share</h1>
+      <input onChange={e => setCompanyId(e.target.value)} value={companyId} />
+
+      <br />
+      <br />
+      <br />
+
+      <ecological-earth id={companyId} />
     </div>
   );
 };
